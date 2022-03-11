f6998445f6f8c5db10b9fec3cba838c1ce30931e
Andrea <andrea.mainardi2002@gmail.com>
2022-03-11 15:55:06 +0100

    NewForm

--- er-building-tool/src/app/app.component.ts
+++ er-building-tool/src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  form = this.fb.group({
+    pesoArmi: [0, Validators.required],
+    pesoTrasportabile: [0, Validators.required],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  emailFormControl = new FormControl('', [Validators.required]);
   title = 'er-building-tool';
+
+  onSubmit() {
+    if (this.form.invalid) {
+      alert("Inserisci tutti i campi");
+    }
+  }
 }
--- er-building-tool/src/app/app.module.ts
+++ er-building-tool/src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +17,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
